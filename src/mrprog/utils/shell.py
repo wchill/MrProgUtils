9257cfbbfa38170393dc41392bdc760f27f858e2
Eric Ahn <wchill1337@gmail.com>
2023-06-01 21:46:00 -0700

    Add shell utilities

---
+++ src/mrprog/utils/shell.py
@@ -0,0 +1,27 @@
+import asyncio
+import os
+import pathlib
+from typing import Optional
+
+GIT_REPOS = ["BattleNetworkAutomation", "BattleNetworkData", "MrProgDiscordBot", "MrProgSwitchWorker", "MrProgUtils"]
+
+
+async def run_shell(cmd: str, cwd: Optional[str] = None) -> str:
+    proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, cwd=cwd)
+    stdout, _ = await proc.communicate()
+    return stdout.decode("utf-8")
+
+
+async def get_git_versions() -> dict[str, str]:
+    path = pathlib.Path(__file__)
+    while not str(path).endswith("MrProgUtils"):
+        path = path.parent
+
+    common_dir = path.parent
+
+    retval = {}
+    for repo_name in GIT_REPOS:
+        git_version = (await run_shell("git describe --always", cwd=str(common_dir / repo_name))).strip()
+        retval[repo_name] = git_version
+
+    return retval
