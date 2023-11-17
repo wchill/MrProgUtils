7d41a216bfcb9bba9b7080fe47e6bc52f68f9046
Eric Ahn <wchill1337@gmail.com>
2023-11-17 00:23:32 -0800

    Use alternative method of retrieving git hashes on Windows

--- src/mrprog/utils/shell.py
+++ src/mrprog/utils/shell.py
@@ -1,33 +1,40 @@
 import asyncio
+import os
 import pathlib
+import subprocess
+import sys
 from typing import Optional
 
 GIT_REPOS = ["BattleNetworkAutomation", "BattleNetworkData", "MrProgDiscordBot", "MrProgWorker", "MrProgUtils"]
 
 
-async def run_shell(cmd: str, cwd: Optional[str] = None) -> str:
+async def run_shell_unix(cmd: str, cwd: Optional[str] = None) -> str:
     proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, cwd=cwd)
     stdout, _ = await proc.communicate()
     return stdout.decode("utf-8")
 
 
+async def run_shell_windows(cmd: str, cwd: Optional[str] = None) -> str:
+    proc = await asyncio.to_thread(lambda: subprocess.run(cmd, stdout=subprocess.PIPE, cwd=cwd))
+    return proc.stdout.decode("utf-8")
+
+
+RUN_SHELL_FUNC = run_shell_windows if sys.platform.lower() == "win32" or os.name.lower() == "nt" else run_shell_unix
+
+
 async def get_git_versions() -> dict[str, str]:
-    try:
-        path = pathlib.Path(__file__)
-        while not str(path).endswith("MrProgUtils"):
-            path = path.parent
+    path = pathlib.Path(__file__)
+    while not str(path).endswith("MrProgUtils"):
+        path = path.parent
 
-        common_dir = path.parent
+    common_dir = path.parent
 
-        retval = {}
-        for repo_name in GIT_REPOS:
-            try:
-                git_version = (await run_shell("git describe --always", cwd=str(common_dir / repo_name))).strip()
-                retval[repo_name] = git_version
-            except FileNotFoundError:
-                pass
+    retval = {}
+    for repo_name in GIT_REPOS:
+        try:
+            git_version = (await RUN_SHELL_FUNC("git describe --always", cwd=str(common_dir / repo_name))).strip()
+            retval[repo_name] = git_version
+        except FileNotFoundError:
+            pass
 
-        return retval
-    except NotImplementedError:
-        # Windows doesn't support running subprocess via asyncio
-        return {}
+    return retval
